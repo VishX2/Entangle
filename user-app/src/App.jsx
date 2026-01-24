7a35be2fc047357517f36c386555c12bb7967321
Banuka Silva <silvabanu183290@gmail.com>
2026-01-24 17:23:01 +0530

    Add EntangleLogin component and update routing in App

--- user-app/src/App.jsx
+++ user-app/src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import AppLayout from './layouts/AppLayout';
 import HomeLayout from './layouts/HomeLayout';
-
+import EntangleLogin from './pages/Login';
 import Home from './pages/Home';
 
 
@@ -13,6 +13,7 @@ function App() {
         {/* HOME (NO SIDEBAR) */}
         <Route element={<HomeLayout />}>
           <Route path="/" element={<Home />} />
+          <Route path="/login" element={<EntangleLogin />} />
         </Route>
 
         

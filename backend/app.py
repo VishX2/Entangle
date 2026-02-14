df0a2f6a50059c8ef022770df80610bfce23e34b
Yehansaa <yehansasandakini@gmail.com>
2026-02-14 12:30:51 +0530

    created startup/dashboard endpoint

--- backend/app.py
+++ backend/app.py
@@ -1,15 +1,18 @@
 from fastapi import FastAPI
-from backend.db import connect_to_mongo, close_mongo
-from backend.routes import entrepreneur
+from db import connect_to_mongo, close_mongo
+from routes import entrepreneur
+from routes import startup
+
 
 app = FastAPI()
 
 @app.on_event("startup")
-async def startup():
+async def startup_event():
     await connect_to_mongo()
 
 @app.on_event("shutdown")
-async def shutdown():
+async def shutdown_event():
     await close_mongo()
 
 app.include_router(entrepreneur.router, prefix="/entrepreneur")
+app.include_router(startup.router, prefix="/startup")   # 👈 ADD THIS

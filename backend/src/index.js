1be2c9b76e3fe468fb2606fd7aed7197d2ef4323
VishX2 <vishwa.amarajith777@gmail.com>
2026-03-04 21:28:41 +0530

    Implemented Reviews & Feedback System

--- backend/src/index.js
+++ backend/src/index.js
@@ -14,6 +14,7 @@ app.use(express.json());
 app.get('/health', (req, res) => res.json({ status: 'ok', service: 'entangle-api' }));
 
 app.use('/api', routes);
+app.use('/', routes);
 
 app.use(errorHandler);
 
